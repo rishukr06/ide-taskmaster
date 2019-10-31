87e250061d18a8f5b0b1af9bce8569027f83e7eb
Faisal Alam <faisal.alam@rentomojo.com>
2019-11-01 01:25:12 +0530

    Remove existing worker image (if any) before pulling one

--- pull_worker_images.sh
+++ pull_worker_images.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+docker image ifaisalalam/ide-worker-c 2> /dev/null
 docker pull ifaisalalam/ide-worker-c
 
+docker image rm ifaisalalam/ide-worker-cpp 2> /dev/null
 docker pull ifaisalalam/ide-worker-cpp
