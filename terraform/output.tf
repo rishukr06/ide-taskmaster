6ed908486c600b34a4aab565191a3c669ec6219c
Faisal Alam <faisal.alam@rentomojo.com>
2020-02-01 02:39:29 +0530

    [Fix] Update Terraform output.tf file

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,7 +1,7 @@
 output "instance_group" {
-  value = google_compute_instance_group_manager.ide_taskmaster_instance_group.instance_group
+  value = google_compute_instance_group_manager.ide_taskmaster_2_instance_group.instance_group
 }
 
 output "instance_group_self_link" {
-  value = google_compute_instance_group_manager.ide_taskmaster_instance_group.self_link
+  value = google_compute_instance_group_manager.ide_taskmaster_2_instance_group.self_link
 }
